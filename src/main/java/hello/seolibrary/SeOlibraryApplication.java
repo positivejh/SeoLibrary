3a38050604ce2d5fe72e1e7d156351dc633b5e8d
jihwan <rlaxorgka12@naver.com>
2024-06-10 15:27:44 +0900

    hi

---
+++ src/main/java/hello/seolibrary/SeOlibraryApplication.java
@@ -0,0 +1,13 @@
+package hello.seolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeOlibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeOlibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/seolibrary/SeOlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.seolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeOlibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
